4b29850f9a6ef6cf5491c58d515dc635b3e4ff94
amSaul <amsaulbosire@gmail.com>
2024-03-25 01:27:54 +0300

    minor changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                  <i class="ri-phone-fill"></i> +254-790-223-386
                 </span>
               </div>
             </Col>
@@ -86,8 +86,8 @@ const Header = () => {
                   <i class="ri-earth-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Bangladesh</h4>
-                  <h6>Sylhet City, Bangladesh</h6>
+                  <h4>kenya</h4>
+                  <h6>Nairobi City, Nairobi</h6>
                 </div>
               </div>
             </Col>
